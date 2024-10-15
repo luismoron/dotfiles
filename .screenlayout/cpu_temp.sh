f244b8f91d7dcfb39087a01256718d17c26d19dc
Luis Moron <leonelmoron@gmail.com>
2024-10-15 07:40:21 -0400

    add my monitor

---
+++ .screenlayout/cpu_temp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Obtener la temperatura de la CPU
+temp=$(sensors | grep 'Tctl' | awk '{print $2}' | tr -d '+°C')
+
+# Verificar si la temperatura se obtuvo correctamente
+if [ -z "$temp" ]; then
+    echo "Error: No se pudo obtener la temperatura"
+    exit 1
+fi
+
+# Formatear la salida para i3status
+echo "CPU Temp: $temp°C"
